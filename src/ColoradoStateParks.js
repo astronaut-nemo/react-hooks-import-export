52659a0bf2546c2c69e44608a4dded53b90581c3
Joanna <astronautnemo@gmail.com>
2023-01-10 13:11:46 +0300

    completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,22 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import * as RMFunctions from "./parks/RockyMountain";
+import {elevation as Elevation} from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
+  RMFunctions.wildlife();
+  console.log(RMFunctions.trees)
+  Elevation();
 
-  return <h1>Colorado State Parks!</h1>;
+  return (
+    <>
+      <h1>Colorado State Parks!</h1>
+      <MesaVerde />
+      
+    </>
+  );
 }
+
+export default ColoradoStateParks;
